2c4d6f8ed4f5d6f40b08006494030c2fca9ba483
matu4869 <13156@hakodate.kosen-ac.jp>
2017-12-15 10:08:10 +0900

    hoge

--- game/Morio.java
+++ game/Morio.java
@@ -14,6 +14,10 @@ public class Morio extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "right" ) ){
+        setRotation(0);
+        move(1);
+    }
+
     }    
 }
