ed62a4f849b75ba975602ef51156f68c1ed854d2
s13123 <13123@hakodate.kosen-ac.jp>
2017-12-20 10:02:01 +0900

    A hoge

--- game/Morio.java
+++ game/Morio.java
@@ -12,19 +12,36 @@ public class Morio extends Actor
      * Act - do whatever the Morio wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int jpmax = 30, jp = 0, high = 0;
     public void act() 
     {
-<<<<<<< HEAD
+
         if( Greenfoot.isKeyDown( "right" ) ){
         setRotation(0);
         move(1);
     }
 
-=======
        if( Greenfoot.isKeyDown( "left" ) ){
         setRotation(0);
         move(-1);
     } // Add your action code here.
->>>>>>> 28c232ee6b7b2c5fa5c6e667620df0aae082eaf8
-    }    
+    
+        if( Greenfoot.isKeyDown( "up" ) ){
+        if(jp == 0 && high == 0){
+            jp = jpmax;
+        }
+    }
+        if(jp > 0){
+        jp--;
+        high++;
+        setRotation(270);
+        move(3);
+    }
+        if(jp == 0 && high > 0){
+        high--;
+        setRotation(90);
+        move(3);
+    }
+    
+   }
 }
