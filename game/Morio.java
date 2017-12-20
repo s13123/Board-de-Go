5fb0df55199e291feb10b72d29847315840cd58e
gluwim2 <13166@hakodate.kosen-ac.jp>
2017-12-20 09:46:47 +0900

    add left

--- game/Morio.java
+++ game/Morio.java
@@ -14,17 +14,13 @@ public class Morio extends Actor
      */
     public void act() 
     {
-<<<<<<< HEAD
         if( Greenfoot.isKeyDown( "right" ) ){
         setRotation(0);
         move(1);
     }
-
-=======
        if( Greenfoot.isKeyDown( "left" ) ){
         setRotation(0);
         move(-1);
-    } // Add your action code here.
->>>>>>> 28c232ee6b7b2c5fa5c6e667620df0aae082eaf8
+    }// Add your action code here.
     }    
 }
