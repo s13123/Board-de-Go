0c05a4ba009061373f2d3643ef1dea6169fd2fee
gluwim2 <13166@hakodate.kosen-ac.jp>
2018-01-24 09:44:40 +0900

    add left

--- game/MyWorld.java
+++ game/MyWorld.java
@@ -6,19 +6,22 @@ import greenfoot.*;
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class MyWorld extends World
 {
-    //private int rtime;
+    private int rtime;
     
-    /*public void act()
+    
+    public void act()
     {    
+        
         rtime--;
         showText( "rtime =" + rtime, 500, 50 );
         if(rtime == 0){
             showText( "time up", 500, 50 );
              Greenfoot.stop();
         }
-    }*/
+    }
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -27,7 +30,8 @@ public class MyWorld extends World
     {    
        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        //rtime=1000;
+        rtime=1000;
+        addObject( new Morio(), 50, 200 );
     }
    
     
